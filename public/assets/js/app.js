b292db5909927fab38af61fd73a32ec62a96cf2f
James Roth <jkltroth@gmail.com>
2018-03-28 22:48:18 -0400

    Added carousel component to projects page

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -7,5 +7,9 @@ $(document).ready(function () {
         coverTrigger: false,
         constrainWidth: false
     });
+    $('.carousel').carousel({
+        shift: 50,
+        padding: 150
+    });
     
 });
